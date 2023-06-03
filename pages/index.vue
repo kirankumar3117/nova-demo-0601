438d22e3aa5926861b9bab82d26b3839d024dbe4
kirankumarhexafoldtech <kirankumar.challagiri@hexafoldtech.com>
2023-06-04 01:42:41 +0530

    nova-demo-created

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,5 @@
 <template>
-  <Tutorial />
+    <div>
+        <h1>Home Page..!</h1>
+    </div>
 </template>
-
-<script lang="ts">
-import Vue from 'vue'
-
-export default Vue.extend({
-  name: 'IndexPage',
-})
-</script>
